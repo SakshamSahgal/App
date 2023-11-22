4b5c9010ce85f719657aa02af43d6316a67c1e66
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2023-11-22 18:21:46 +0530

    organised file structure

--- App.js
+++ App.js
@@ -1,32 +1,7 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import HomeScreen from "./src/Screens/HomeScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.textBox}
-        placeholder="Text Box 1"
-      />
-      <TextInput
-        style={styles.textBox}
-        placeholder="Text Box 2"
-      />
-    </View>
+    <HomeScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingHorizontal: 16,
-  },
-  textBox: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 16,
-    paddingHorizontal: 10,
-  },
-});
---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,33 @@
+// HomeScreen.js
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textBox}
+        placeholder="Text Box 1"
+      />
+      <TextInput
+        style={styles.textBox}
+        placeholder="Text Box 2"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+  },
+  textBox: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+  },
+});
